adb1ff4f97f8c4d98c1c710974b5ea1b3e061567
Koen Reinders <koenf.reinders@gmail.com>
2026-07-03 17:30:14 +0200

    test: temporarily replace scheduler config for testing and add dummy capture script

--- scripts/scheduling/config.py
+++ scripts/scheduling/config.py
@@ -10,22 +10,38 @@ from zoneinfo import ZoneInfo
 class SchedulerConfig:
     config_path: Path
     state_path: Path
-    capture_script: str
-    python_bin: str
+    capture_script: Path
+    python_bin: Path
     output_dir: Path
     max_lateness: timedelta
     poll_interval: float
     tz: ZoneInfo
 
 
+# def default_scheduler_config() -> SchedulerConfig:
+#     return SchedulerConfig(
+#         config_path=Path("/home/phenopi/phenotyping/schedule.json"),
+#         state_path=Path("/home/phenopi/phenotyping/completed_jobs.json"),
+#         capture_script="/home/phenopi/phenotyping/capture_once.py",
+#         python_bin="/home/phenopi/venvs/phenopi/bin/python",
+#         output_dir=Path("/home/phenopi/captures"),
+#         max_lateness=timedelta(minutes=10),
+#         poll_interval=20.0,
+#         tz=ZoneInfo("Europe/Amsterdam"),
+#     )
+
+
 def default_scheduler_config() -> SchedulerConfig:
+    testpath = Path(
+        "/home/koen/Documents/MSc Biology/Y2/2b2-3_research_project_2/phenopi/"
+    )
     return SchedulerConfig(
-        config_path=Path("/home/phenopi/phenotyping/schedule.json"),
-        state_path=Path("/home/phenopi/phenotyping/completed_jobs.json"),
-        capture_script="/home/phenopi/phenotyping/capture_once.py",
-        python_bin="/home/phenopi/venvs/phenopi/bin/python",
-        output_dir=Path("/home/phenopi/captures"),
+        config_path=testpath / "runtime/schedule.json",
+        state_path=testpath / "runtime/completed_jobs.json",
+        capture_script=testpath / "scripts/capture/dummy_capture.py",
+        python_bin=Path("/home/koen/Downloads/plantcv/bin/python"),
+        output_dir=testpath / "captures",
         max_lateness=timedelta(minutes=10),
-        poll_interval=20.0,
+        poll_interval=5.0,
         tz=ZoneInfo("Europe/Amsterdam"),
     )
